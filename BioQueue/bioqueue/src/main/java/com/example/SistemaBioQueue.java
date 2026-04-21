49f815e3ddd012d1d6ea2b38b8ccf90f020cd128
RodrigoCardinal <rodrigo.cardinal@correo.ucu.edu.uy>
2026-04-21 18:19:02 -0300

    Por ahora queda el metodo de desempate

--- BioQueue/bioqueue/src/main/java/com/example/SistemaBioQueue.java
+++ BioQueue/bioqueue/src/main/java/com/example/SistemaBioQueue.java
@@ -1,8 +1,35 @@
 package com.example;
+import java.util.Random;
+import com.example.ImplementacionesTDA.ListaEnlazada;
 
 public class SistemaBioQueue {
-    private Lista_Donantes lista;
-    public SistemaBioQueue(String donantes) {
-        lista=new Lista_Donantes();
+/* 
+    private ListaEnlazada<Receptor> lista_desempate;
+    private ListaEnlazada<Receptor> lista_receptores;
+
+    public SistemaBioQueue() {
+        this.lista_desempate = new ListaEnlazada<>();
+        this.lista_receptores = new ListaEnlazada<>();
     }
+
+
+
+    public void desempateRandom() {
+        int totalEmpatados = lista_desempate.tamaño();
+
+        if (totalEmpatados == 0) {
+            System.out.println("No hay receptores en la lista de desempate.");
+            return;
+        }
+
+        Random random = new Random();
+        int indiceSeleccionado = random.nextInt(0,totalEmpatados);  //Sabemos que el random empieza desde cero pero decidimos ponerlo solo para enderderlo mejor
+        Receptor receptorSeleccionado = lista_desempate.obtener(indiceSeleccionado);
+
+        System.out.println("DESEMPATE POR RANDOM:");
+        System.out.println("   Lista de empatados: " + totalEmpatados + " receptores");
+        System.out.println("   Índice seleccionado: " + indiceSeleccionado);
+        System.out.println("   Receptor seleccionado: " + receptorSeleccionado.getNombre() + " (Cédula: " + receptorSeleccionado.getCedula() + ", Órgano necesario: " + receptorSeleccionado.getOrgano_necesitado() + ", Prioridad: " + receptorSeleccionado.getPrioridad() + ")");
+    }
+        */
 }
