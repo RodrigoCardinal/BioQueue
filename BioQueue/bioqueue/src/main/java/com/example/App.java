b4cb2875edde666297c6c748691064f22b39a129
risso <valentina.risso@correo.ucu.edu.uy>
2026-05-02 22:38:58 -0300

    agrego fachada

--- BioQueue/bioqueue/src/main/java/com/example/App.java
+++ BioQueue/bioqueue/src/main/java/com/example/App.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class App {
+
     public static void main(String[] args) {
+        /* 
         // Cargar receptores
         SistemaBioQueue sistemaBioQueue=new SistemaBioQueue();
         GestorReceptores listaReceptores = sistemaBioQueue.getGestorReceptores();
@@ -10,7 +14,7 @@ public class App {
         listaReceptores.MostrarReceptores();
 
         // Cargar donantes
-        GestorDonantes listaDonantes = new GestorDonantes();
+        GestorDonantes listaDonantes = sistemaBioQueue.getGestorDonantes();
         listaDonantes.cargarDonantes("BioQueue\\bioqueue\\src\\Resources\\Donantes.csv");
         System.out.println("\n=== DONANTES CARGADOS ===");
         listaDonantes.MostrarDonantes();
@@ -31,5 +35,86 @@ public class App {
 
         System.out.println("\n=== RECEPTORES EN ESPERA (NO ASIGNADOS) ===");
         listaReceptores.MostrarReceptores();
+
+
+
+    1. Registrar receptor
+    2. Registrar donante
+    3. Buscar receptor
+    4. Buscar donante
+    5. Eliminar receptor
+    6. Eliminar donante
+    7. Listar receptores
+    8. Listar donantes
+    9. Listar órganos disponibles
+    10. Ver historial de transplantes
+    -1. Salir
+
+    opcion ← leer entero
+
+    SEGÚN opcion:
+      1  → REGISTRAR_RECEPTOR()
+      2  → REGISTRAR_DONANTE()
+      3  → Leer cedula; BUSCAR_RECEPTOR(cedula)
+      4  → Leer cedula; BUSCAR_DONANTE(cedula)
+      5  → Leer cedula; ELIMINAR_RECEPTOR(cedula)
+      6  → Leer cedula; ELIMINAR_DONANTE(cedula)
+      7  → LISTAR_RECEPTORES()
+      8  → LISTAR_DONANTES()
+      9  → LISTAR_ORGANOS_DISPONIBLES()
+      10 → mostrar historialTransplantes
+      11 → SALIR
+
+         */
+        Facade facade = Facade.Instancia();
+        String opcion = "";
+        while (!opcion.equals("-1")) {
+            facade.mostrarMenu();
+            Scanner sc = new Scanner(System.in);
+            opcion = sc.nextLine();
+            switch (opcion) {
+                case "-1":
+                    break;
+                case "1":
+                    Facade.Instancia().registrarDonante();
+                    break;
+                case "2":
+                    Facade.Instancia().cargarDonantesDesdeArchivo();
+                    break;
+                case "3":
+                    Facade.Instancia().buscarDonante();
+                    break;
+                case "4":
+                    Facade.Instancia().eliminarDonante();
+                    break;
+                case "5":
+                    Facade.Instancia().mostrarDonantes();
+                    break;
+                case "6":
+                    Facade.Instancia().registrarReceptor();
+                    break;
+                case "7":
+                    Facade.Instancia().cargarReceptoresDesdeArchivo();
+                    break;
+                case "8":
+                    Facade.Instancia().buscarReceptor();
+                    break;
+                case "9":
+                    Facade.Instancia().eliminarReceptor();
+                    break;
+                case "10":
+                    Facade.Instancia().mostrarReceptores();
+                    break;
+                case "11":
+                    Facade.Instancia().procesoMatch();
+                    break;
+                case "12":
+                    Facade.Instancia().mostrarTransplantes();
+                    break;
+                default:
+                    System.out.print("Opción no válida\r\n");
+
+            }
+        }
     }
 }
